c5aa5f3a36dff929293ce51aaf1a189826106997
vincent anjiri <vincentanjiri12@gmail.com>
2024-08-23 19:39:27 +0300

    "Added state and API call to FeedbackPage to load and display individual feedback details on button click"

--- pages/feedback/index.js
+++ pages/feedback/index.js
@@ -1,12 +1,29 @@
+import { Fragment, useState } from "react";
 import { buildFeedbackPath, extractFeedback } from "../api/feedback";
 
 function FeedBackPage(props) {
+  const [feedbackData, setFeedbackData] = useState();
+  function loadFeedbackHanlder(id) {
+    fetch(`/api/${id}`)
+      .then((response) => response.json())
+      .then((data) => {
+        setFeedbackData(data.feedback);
+      });
+  }
   return (
-    <ul>
-      {props.feedbackItems.map((item) => (
-        <li key={item.id}>{item.text}</li>
-      ))}
-    </ul>
+    <Fragment>
+      {feedbackData && <p>{feedbackData.text}</p>}
+      <ul>
+        {props.feedbackItems.map((item) => (
+          <li key={item.id}>
+            {item.text}
+            <button onClick={loadFeedbackHanlder.bind(null, item.id)}>
+              Show Details
+            </button>
+          </li>
+        ))}
+      </ul>
+    </Fragment>
   );
 }
 
